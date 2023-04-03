75a2ea3d9215a4a89e6b6b73bf876da6236b7590
TaniaZaitouny <taniazaiitouny@gmail.com>
2023-04-03 21:14:53 +0300

    created views

---
+++ resources/views/Partials/styles.blade.php
@@ -0,0 +1,15 @@
+
+
+            <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/flaticon.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/price_rangs.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/slicknav.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/magnific-popup.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/fontawesome-all.min.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/slick.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/nice-select.css') }}" rel="stylesheet">
+            <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+            
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+         <title>Job board HTML-5 Template </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="site.webmanifest">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+        @include('Partials.styles')
+</head>
+<body>
+@include('Partials.header')
+<main >
+    @yield('content')
+</main>
+@include('Partials.footer')
+@include('Partials.scripts')
+</body>
+
+
+
+
+
+
+
+
+
+</html>
