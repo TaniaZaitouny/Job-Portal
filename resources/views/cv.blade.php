710d2728bd78010a3f528152902c490dc3970bb3
Tracy Matta <tracygmatta@gmail.com>
2023-04-07 00:11:47 +0300

    buttons with js don't work

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -7,10 +7,97 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
 
-
-
 </header>
 <body>
+    <script>
+        alert("hi");
+        document.getElementById("add_education").addEventListener("click", function add_education_row() {
+            var education_container = document.getElementById("education_container");
+            alert("hi");
+            alert("<div class=\"row\"\>
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"certificate\" class=\"text-muted\">Degree/Certificate</label\>
+                                        <input id=\"certificate\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"year\" class=\"text-muted\"\>Year</label\>
+                                        <input id=\"year\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+                            </div>");
+            var new_row = document.createElement("<div class=\"row\"\>
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"certificate\" class=\"text-muted\">Degree/Certificate</label\>
+                                        <input id=\"certificate\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"year\" class=\"text-muted\"\>Year</label\>
+                                        <input id=\"year\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+                            </div>");
+            education_container.appendChild("<div class=\"row\"\>
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"certificate\" class=\"text-muted\">Degree/Certificate</label\>
+                                        <input id=\"certificate\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+
+                                <div class=\"col-md-6\"\>
+                                    <div class=\"form-group app-label\"\>
+                                        <label for=\"year\" class=\"text-muted\"\>Year</label\>
+                                        <input id=\"year\" type=\"text\" class=\"form-control resume\"\>
+                                    </div\>
+                                </div\>
+                            </div>");
+        });
+
+        function add_work_row() {
+            var work_container = document.getElementById("work_container");
+            var new_row = document.createElement("<div class=\"row\"\>
+                                    <div class=\"col-md-6\"\>
+                                        <div class=\"form-group app-label\"\>
+                                            <label for=\"company-name\" class=\"text-muted\">Company Name</label\>
+                                            <input id=\"company-name\" type=\"text\" class=\"form-control resume\"\>
+                                        </div\>
+                                    </div\>
+
+                                    <div class=\"col-md-6\"\>
+                                        <div class=\"form-group app-label\"\>
+                                            <label for=\"job-position\" class=\"text-muted\">Job Position</label\>
+                                            <input id=\"job-position\" type=\"text\" class=\"form-control resume\"\>
+                                        </div\>
+                                    </div\>
+
+                                    <div class=\"col-lg-6\"\>
+                                        <div class=\"row\"\>
+                                            <div class=\"col-md-6\"\>
+                                                <div class=\"form-group app-label\"\>
+                                                    <label for=\"date-from\" class=\"text-muted\">Year From</label\>
+                                                    <input id=\"date-from\" type=\"text\" class=\"form-control resume\"\>
+                                                </div\>
+                                            </div\>
+
+                                            <div class=\"col-md-6\"\>
+                                                <div class=\"form-group app-label\"\>
+                                                    <label for=\"date-to\" class=\"text-muted\">Year To</label\>
+                                                    <input id=\"date-to\" type=\"text\" class=\"form-control resume\"\>
+                                                </div\>
+                                            </div\>
+                                        </div\>
+                                    </div\> </div>");
+            work_container.appendChild(new_row);
+        }
+    </script>
 
   @include('Partials.header')
 
@@ -34,28 +121,28 @@
                             <div class="col-md-4">
                                 <div class="form-group app-label">
                                     <label for="frist-name" class="text-muted">First Name</label>
-                                    <input id="frist-name" type="text" class="form-control resume" placeholder="">
+                                    <input id="frist-name" type="text" class="form-control resume">
                                 </div>
                             </div>
 
                             <div class="col-md-4">
                                 <div class="form-group app-label">
                                     <label for="middle-name" class="text-muted">Middle Name</label>
-                                    <input id="middle-name" type="text" class="form-control resume" placeholder="">
+                                    <input id="middle-name" type="text" class="form-control resume">
                                 </div>
                             </div>
 
                             <div class="col-md-4">
                                 <div class="form-group app-label">
                                     <label for="surname-name" class="text-muted">Last Name</label>
-                                    <input id="surname-name" type="text" class="form-control resume" placeholder="">
+                                    <input id="surname-name" type="text" class="form-control resume">
                                 </div>
                             </div>
 
                             <div class="col-md-4">
                                 <div class="form-group app-label">
                                     <label for="date-of-birth" class="text-muted">Date Of Birth</label>
-                                    <input id="date-of-birth" type="text" class="form-control resume" placeholder="13-02-1999">
+                                    <input id="date-of-birth" type="text" class="form-control resume" placeholder="DD-MM-YYYY">
                                 </div>
                             </div>
 
@@ -67,7 +154,7 @@
                                             <option value="Male">Male</option>
                                             <option value="Female">Female</option>
                                             <option value="Other">Other</option>
-                                            <option value="Prefer not to specify" selected>Other</option>
+                                            <option value="Prefer not to specify" selected>Prefer not to specify</option>
                                         </select>
                                     </div>
                                 </div>
@@ -137,25 +224,32 @@
             <h3 class="mt-5" style="color:#28395a">Education Details</h3>
         </div>
     </div>
-
+    
     <div class="row">
         <div class="col-lg-12">
             <div class="job-detail mt-2 p-4">
                 <div class="custom-form">
                     <form>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group app-label">
-                                    <label for="certificate" class="text-muted">Degree/Certificate</label>
-                                    <input id="certificate" type="text" class="form-control resume">
+                        <div id="education_container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group app-label">
+                                        <label for="certificate" class="text-muted">Degree/Certificate</label>
+                                        <input id="certificate" type="text" class="form-control resume">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group app-label">
+                                        <label for="year" class="text-muted">Year</label>
+                                        <input id="year" type="text" class="form-control resume">
+                                    </div>
                                 </div>
                             </div>
-
+                        </div>
+                        <div class="row">
                             <div class="col-md-6">
-                                <div class="form-group app-label">
-                                    <label for="year" class="text-muted">Year</label>
-                                    <input id="year" type="text" class="form-control resume">
-                                </div>
+                                <button type="button" id="add_education" class="btn btn-light">Add Row</button>
                             </div>
                         </div>
                     </form>
@@ -170,70 +264,84 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="job-detail mt-2 p-4">
-                <div class="custom-form">
-                    <form>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group app-label">
-                                    <label for="company-name" class="text-muted">Company Name</label>
-                                    <input id="company-name" type="text" class="form-control resume">
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="form-group app-label">
-                                    <label for="job-position" class="text-muted">Job Position</label>
-                                    <input id="job-position" type="text" class="form-control resume">
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="job-detail mt-2 p-4">
+                    <div class="custom-form">
+                        <form>
+                            <div id="work_container">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label">
-                                            <label for="date-from" class="text-muted">Year From</label>
-                                            <input id="date-from" type="text" class="form-control resume">
+                                            <label for="company-name" class="text-muted">Company Name</label>
+                                            <input id="company-name" type="text" class="form-control resume">
                                         </div>
                                     </div>
 
                                     <div class="col-md-6">
                                         <div class="form-group app-label">
-                                            <label for="date-to" class="text-muted">Year To</label>
-                                            <input id="date-to" type="text" class="form-control resume">
+                                            <label for="job-position" class="text-muted">Job Position</label>
+                                            <input id="job-position" type="text" class="form-control resume">
                                         </div>
                                     </div>
+
+                                    <div class="col-lg-6">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group app-label">
+                                                    <label for="date-from" class="text-muted">Year From</label>
+                                                    <input id="date-from" type="text" class="form-control resume">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group app-label">
+                                                    <label for="date-to" class="text-muted">Year To</label>
+                                                    <input id="date-to" type="text" class="form-control resume">
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div> 
                                 </div>
-                            </div>
-                        </div>
-                    </form>
+                            </div>  
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <button type="button" id="add_work" onclick="add_work_row()" class="btn btn-light">Add Row</button>
+                                </div>
+                            </div>                     
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
     <div class="row">
         <div class="col-lg-12">
             <h3 class="mt-5" style="color:#28395a">Skills</h3>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="job-detail mt-2 p-4">
-                <div class="custom-form">
-                    <form>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="form-group app-label">
-                                    <label for="skill" class="text-muted">Skill</label>
-                                    <input id="skill" type="text" class="form-control resume">
+    <div id="skill_container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="job-detail mt-2 p-4">
+                    <div class="custom-form">
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group app-label">
+                                        <label for="skill" class="text-muted">Skill</label>
+                                        <input id="skill" type="text" class="form-control resume">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <button type="button" id="add_skill" class="btn btn-light">Add Row</button>
+                                </div>
+                            </div> 
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -251,7 +359,9 @@
 </div>
 
 
-
+<script type="text/javascript">
+    
+</script>
 
 
 </body>
